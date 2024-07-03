28aabee1e6115ed6820a0d9d968831ba05519f34
golubb <gila8120@gmail.com>
2024-07-03 16:09:38 +0300

    add function 'sort_by_date' and small fixes in function 'filter_by_state'

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,16 @@
-from typing import Union
+from typing import Any
 
 
-def filter_by_state(new_dict: Union[list]) -> Union[list]:
-    clear_new_dict = []
-    for i in new_dict:
-        if i['state'] == 'EXECUTED':
-            clear_new_dict.append(i)
-    return clear_new_dict
+def filter_by_state(new_list: list[dict[str:Any]], state_id: str = 'EXECUTED') -> Any:
+    """Функция проверяет словари в списке на статус 'state', и оставляет значения только со статусом 'EXECUTED'"""
+    clear_new_list = []
+    for i in new_list:
+        if i['state'] == state_id:
+            clear_new_list.append(i)
+    return clear_new_list
 
 
+def sort_by_date(date_list: list[dict[str:Any]], reverse: bool = True) -> Any:
+    """Функция сортирует список словарей по дате"""
+    sorted_list_date = sorted(date_list, key=lambda date_list: date_list['date'], reverse=reverse)
+    return sorted_list_date
