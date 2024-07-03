6219d62116655a0b738002a94fde18eda8eb7d07
golubb <gila8120@gmail.com>
2024-07-03 17:23:11 +0300

    rename feature brench

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
 from typing import Any
 
 
-def filter_by_state(new_list: list[dict[str:Any]], state_id: str = 'EXECUTED') -> Any:
+def filter_by_state(new_list: list[dict[str:Any]], state_id: str = "EXECUTED") -> Any:
     """Функция проверяет словари в списке на статус 'state', и оставляет значения только со статусом 'EXECUTED'"""
     clear_new_list = []
     for i in new_list:
-        if i['state'] == state_id:
+        if i["state"] == state_id:
             clear_new_list.append(i)
     return clear_new_list
 
 
 def sort_by_date(date_list: list[dict[str:Any]], reverse: bool = True) -> Any:
     """Функция сортирует список словарей по дате"""
-    sorted_list_date = sorted(date_list, key=lambda date_list: date_list['date'], reverse=reverse)
+    sorted_list_date = sorted(date_list, key=lambda date_list: date_list["date"], reverse=reverse)
     return sorted_list_date
